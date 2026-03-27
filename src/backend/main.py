2258c9c1138bd8d56ba34ad72fd8b22fb4460fd9
vento11b <vento11b@gmail.com>
2026-03-27 14:58:55 +0100

    methode-inscription

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, Response,send_from_directory
-from app import db, session
+from app import users, session
 
 
 flask = Flask(__name__)
@@ -39,8 +39,8 @@ def profile():
     # Gestion de compte
     pass
 
-@flask.route("/identifier", methods=["GET", "POST"])
-def identifier():
+@flask.route("/inscription", methods=["GET", "POST"])
+def inscription():
     resp = Response()
     
     # Si la methode de la requete est GET:
@@ -51,9 +51,65 @@ def identifier():
             # Si la cookie 'session_id' est valide, redireiger vers l'application:
 
             resp.status_code = 302
-            resp.location = "/identifier"
+            resp.location = "/app"
         else:
-            resp.data = open("src/frontend/identifier/conexion.html", "r").read()
+            resp.data = open("src/frontend/inscription.html", "r").read()
+
+    # Si la methode de la requete est POST:
+    else:
+        resp.data = {'status': 0, 'info': ''}
+        resp.content_type = "application/json"
+        resp.status_code = 302
+
+        # Recevoir les donnes pour l'inscription:
+        username, password = request.json["username"], request.json["password"]
+            
+        if users.check_password():
+            if users.check_username():
+                # Creer utilisateur
+                if users.create_user(username, password):
+                    ## Creer une session
+                    #session_id = session.create_session(username)
+                    ## Creer la cookie
+                    #resp.set_cookie("session_id", session_id)
+                    resp.data = {'status': 0, 'info': 'Compte cree.'}
+                    resp.location = "/1"
+                else:
+                    resp.data = {'status': 0, 'info': 'Error dans la creation du compte'}
+                    resp.location = "/inscription"
+            else:
+                resp.data = {'status': 0, 'info': 'Le nom d\'utilisateur n\'est pas disponible.'}
+                resp.location = "/inscription"
+        else:
+            resp.data = {'status': 0, 'info': 'Le mot de passe ne remplit pas les conditions.'}
+            resp.location = "/inscription"
+    
+    resp.data = str(resp.data).encode()
+    
+    return resp
+
+@flask.route("/connexion", methods=["GET", "POST"])
+def connexion():
+    return
+    resp = Response()
+
+    # Creer une session
+    session_id = session.create_session(username)
+    # Creer la cookie
+    resp.set_cookie("session_id", session_id)
+    # Si la methode de la requete est GET:
+
+    if request.method == "GET":
+
+        
+        if session_id := request.cookies.get("session_id"):
+            # Si la cookie 'session_id' est valide, redireiger vers l'application:
+
+            resp.status_code = 302
+            resp.location = "/connexion"
+        else:
+            # Sinon envoyer page HTML
+            resp.data = open("src/frontend/connexion.html", "r").read()
 
     # Si la methode de la requete est POST:
     else:
@@ -74,7 +130,6 @@ def identifier():
             resp.data = b"{'status': 0}"
             resp.content_type = "application/json"
     return resp
-
 
 if __name__ == '__main__':
     flask.run()
