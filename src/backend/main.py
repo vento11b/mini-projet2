9582e4f9e9b4be4772598e691d11036281f28688
Yoann_Gnangla <yoann.gnangla@stfelixlasalle.fr>
2026-03-31 19:46:31 +0200

    lancer et html

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,8 +2,8 @@ from flask import Flask, request, Response,send_from_directory
 from app import appdb, session
 import json
 
-from urllib import urlencode # -> python2 
-#from urllib.parse import urlencode # -> python3
+#from urllib import urlencode # -> python2 
+from urllib.parse import urlencode # -> python3
 
 flask = Flask(__name__)
 
